178fc41b1e37d57cd4d1647af8012fe86db46e5e
cbmartinez42 <cbmartinez42@gmail.com>
2021-05-08 20:55:07 -0500

    confirming connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,10 @@
 
+// let sequelize;
 
-
-let sequelize;
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+  let sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  sequelize = new Sequelize(
+  let sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PW,
